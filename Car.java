1f8e3f52146020b151aa08232b1e092a4838ee25
melkerr1 <melkerr@chalmers.se>
2024-01-22 14:07:11 +0100

    first commit

---
+++ Car.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+
+public class Car{
+
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+    
+    public Car(int nrDoors, Color color, double enginePower, String modelName){
+        this.nrDoors = nrDoors;
+        this.color = color;
+        this.enginePower = enginePower;
+        this.modelName = modelName;
+        stopEngine();
+    }
+    
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+
+    public double speedFactor(){
+        return enginePower * 0.01;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+    
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+}
