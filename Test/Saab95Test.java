f187550b1ca510ce78d0ceb3a80b57dac49a9a78
Isak Sixten <isak.sixten@gmail.com>
2024-01-26 16:59:55 +0100

    comment removed

--- Test/Saab95Test.java
+++ Test/Saab95Test.java
@@ -52,16 +52,5 @@ public class Saab95Test{
         TestSaab.setTurboOff();
         assertEquals(1.25,TestSaab.speedFactor());
     }
-    // @Test
-    // public void saabincrementspeed() {
-    //     TestSaab.incrementSpeed(100);
-    //     assertEquals(125,TestSaab.getCurrentSpeed());
-    // }
-    // @Test
-    // public void saabdecrementspeed() {
-    //     TestSaab.decrementSpeed(100);
-    //     assertEquals(0,TestSaab.getCurrentSpeed());
-    // }
-    // @Test
 }
 
